c3933ecccc87f0359212dd3a72a6938a76bae947
hwdtw <yar0730@msn.com>
2024-05-15 10:56:59 +0800

    feat/ plugin.yml config, register plugin and set command executor.

--- InvitsPlugin/src/main/java/com/rock_mc/invitsplugin/InvitsPlugin.java
+++ InvitsPlugin/src/main/java/com/rock_mc/invitsplugin/InvitsPlugin.java
@@ -4,9 +4,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InvitsPlugin extends JavaPlugin {
 
+    public static final String APP_NAME = "InvitSys";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        Log.logger = getLogger();
+
+        Log.server("Enable", "Active");
+
+        getServer().getPluginManager().registerEvents(new EventListener(), this);
+        this.getCommand("invits").setExecutor(new PlayerCommand());
+
+
+        Log.server("Enable", "Complete");
 
     }
 
@@ -14,4 +26,6 @@ public final class InvitsPlugin extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+
 }
