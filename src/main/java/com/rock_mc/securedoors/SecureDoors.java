6b67cc184b64f6a5700b1182f0ce50b92f9aa739
hwdtw <yar0730@msn.com>
2024-05-16 23:25:27 +0800

    feat: sqlite database support

--- src/main/java/com/rock_mc/securedoors/SecureDoors.java
+++ src/main/java/com/rock_mc/securedoors/SecureDoors.java
@@ -1,18 +1,36 @@
 package com.rock_mc.securedoors;
 
+import com.rock_mc.securedoors.database.SdDatabase;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.SQLException;
 
 public class SecureDoors extends JavaPlugin {
 
     public static final String APP_NAME = "SecureDoors";
 
+    private SdDatabase db;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
         Log.logger = getLogger();
 
-        getServer().getPluginManager().registerEvents(new EventListener(), this);
+        try {
+            // Ensure the plugin's data folder exists
+            if (!getDataFolder().exists()) {
+                getDataFolder().mkdirs();
+            }
+
+            db = new SdDatabase(getDataFolder().getAbsolutePath() + "/secureDoors.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Failed to connect to database! " + e.getMessage());
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+
+        Bukkit.getPluginManager().registerEvents(new EventListener(), this);
+
         this.getCommand("sd").setExecutor(new PlayerCommand());
 
         Log.server("Enable", "Complete");
@@ -21,7 +39,15 @@ public class SecureDoors extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        try {
+            db.closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public SdDatabase getSdDatabase() {
+        return this.db;
     }
 
 
---
+++ src/main/java/com/rock_mc/securedoors/database/SdDatabase.java
@@ -0,0 +1,74 @@
+package com.rock_mc.securedoors.database;
+
+import org.bukkit.entity.Player;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SdDatabase {
+
+    private final Connection connection;
+
+    public SdDatabase(String path) throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:" + path);
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("CREATE TABLE IF NOT EXISTS whitelist (" +
+                    "uuid TEXT PRIMARY KEY, " +
+                    "username TEXT NOT NULL, " +
+                    "invitationQuota INTEGER NOT NULL DEFAULT 0)");
+        }
+    }
+
+    public void closeConnection() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    public void addPlayerToWhiteList(Player player) throws SQLException {
+        //this should error if the player already exists
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO whitelist (uuid, username) VALUES (?, ?)")) {
+            preparedStatement.setString(1, player.getUniqueId().toString());
+            preparedStatement.setString(2, player.getName());
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public boolean playerExistsInWhiteList(Player player) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM whitelist WHERE uuid = ?")) {
+            preparedStatement.setString(1, player.getUniqueId().toString());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        }
+    }
+
+    public void updatePlayerInvitationQuota(Player player, int invitationQuota) throws SQLException{
+
+        //if the player doesn't exist, add them
+        if (!playerExistsInWhiteList(player)){
+            addPlayerToWhiteList(player);
+        }
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement("UPDATE whitelist SET invitationQuota = ? WHERE uuid = ?")) {
+            preparedStatement.setInt(1, invitationQuota);
+            preparedStatement.setString(2, player.getUniqueId().toString());
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public int getPlayerInvitationQuota(Player player) throws SQLException {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT invitationQuota FROM whitelist WHERE uuid = ?")) {
+            preparedStatement.setString(1, player.getUniqueId().toString());
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("invitationQuota");
+            } else {
+                return 0; // Return 0 if the player has no invitationQuota
+            }
+        }
+    }
+}
