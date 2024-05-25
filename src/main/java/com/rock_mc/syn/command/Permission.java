81ec4a16b82fb81a3ec6d9f24e9b6db0ab896139
hwdtw <yar0730@msn.com>
2024-05-25 21:49:06 +0800

    Feat/tab complete func (#24)
    * test: system console log using Bukkit.getLogger().info("...") for logging, Bukkit.getConsoleSender().sendMessage("...") for testing
    
    * test: disable PlayerJoinEvent before test
    
    * test: wrong verify code
    
    * feat: tab complete func
    
    * test: refine test case after merge
    
    * fix: follow the usage of current readme and plugin.yml
    
    * chore: adjust default cmd.
    
    * refactor: the cmd and the permission code.
    
    * test: sorted and matched commands; server log
    
    ---------
    
    Co-authored-by: CodingMan <pttcodingman@gmail.com>

--- src/main/java/com/rock_mc/syn/command/Permission.java
+++ src/main/java/com/rock_mc/syn/command/Permission.java
@@ -1,10 +1,11 @@
-package com.rock_mc.syn;
+package com.rock_mc.syn.command;
 
 public class Permission {
+    public static String VERIFY = "syn.verify";
     public static String GENCODE = "syn.gencode";
     public static String BAN = "syn.ban";
     public static String UNBAN = "syn.unban";
-    public static String INFO = "syn.info";
     public static String GUEST = "syn.guest";
+    public static String INFO = "syn.info";
     public static String LOG = "syn.log";
 }
