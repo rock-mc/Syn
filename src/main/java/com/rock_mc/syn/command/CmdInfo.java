ab966b380a3f4ac3684949d8ca9ae934895252da
hwdtw <yar0730@msn.com>
2024-05-28 16:23:34 +0800

    feat: syn info and unban commands #32

--- src/main/java/com/rock_mc/syn/command/CmdInfo.java
+++ src/main/java/com/rock_mc/syn/command/CmdInfo.java
@@ -2,12 +2,35 @@ package com.rock_mc.syn.command;
 
 import com.rock_mc.syn.Syn;
 import com.rock_mc.syn.log.LogProvider;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
+
+import java.text.SimpleDateFormat;
 
 public class CmdInfo {
     private final static String commandName = CmdManager.INFO;
 
     public static void run(Syn plugin, LogProvider log, Player player, String[] args) {
-        // TODO: syn info [player]
+        PlayerInfo playerInfo = null;
+        if (args.length == 2) {
+            String playerName = args[1];
+
+            log.sendMessage(player, "查詢使用者" + ChatColor.GREEN + playerName);
+
+            // 取得玩家資料
+            playerInfo = plugin.dbManager.getPlayerByName(playerName);
+            if (playerInfo == null) {
+                log.sendMessage(player, "查無此玩家" + playerName);
+
+            } else {
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm");
+
+                log.sendMessage(player, "最近登入" + sdf.format(playerInfo.getLast_login()));
+                log.sendMessage(player, "加入時間" + sdf.format(playerInfo.getCreated_at()));
+
+            }
+        } else {
+            log.sendMessage(player, "Syn version: " + plugin.getServer().getVersion());
+        }
     }
 }
--- src/main/java/com/rock_mc/syn/command/CmdUnban.java
+++ src/main/java/com/rock_mc/syn/command/CmdUnban.java
@@ -2,12 +2,26 @@ package com.rock_mc.syn.command;
 
 import com.rock_mc.syn.Syn;
 import com.rock_mc.syn.log.LogProvider;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 
 public class CmdUnban {
     private final static String commandName = CmdManager.UNBAN;
 
     public static void run(Syn plugin, LogProvider log, Player player, String[] args) {
-        // TODO: syn unban <player>
+        String unblockPlayerName = args[1];
+        log.sendMessage(player, "將使用者移出黑名單" + ChatColor.GREEN + unblockPlayerName);
+
+
+        PlayerInfo unblockPlayer = plugin.dbManager.getPlayerByName(unblockPlayerName);
+        if (unblockPlayer == null) {
+            log.sendMessage(player, "查無此玩家" + unblockPlayerName);
+            return;
+        }
+
+        plugin.dbManager.removePlayerBannedList(unblockPlayer.getPlayer_uuid());
+
+        log.sendMessage(player, "執行狀態" + ChatColor.GREEN + "完成");
+
     }
 }
